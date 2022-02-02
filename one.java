eeb3bac99089f09b0a0c98617ab5fee6db0a4ac9
Gautam Nayak <88781537+gautamnayak522@users.noreply.github.com>
2022-02-02 16:04:33 +0530

    Closes #2

---
+++ one.java
@@ -0,0 +1,8 @@
+class InfiniteForLoopDemo
+{
+public static void main(String[] arg) {
+for(;;)
+System.out.println("Welcome to ABCD!");
+// To terminate this program press ctrl + c in the console.
+}
+}
