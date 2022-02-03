b9506608f445351bd637f12604bdcebcec295220
Gautam Nayak <88781537+gautamnayak522@users.noreply.github.com>
2022-02-03 14:01:57 +0530

    changes

--- One.java
+++ One.java
@@ -2,7 +2,7 @@ class InfiniteForLoopDemo
 {
 public static void main(String[] arg) {
 for(;;)
-System.out.println("Welcome to ABCDEFGHIJKL!");
+System.out.println("Welcome to alpha - ABCDEFGHIJKL!");
 // To terminate this program press ctrl + c in the console.
 }
 }
