8753bc44e2d1382ca218e078aa165bd62107160b
Gautam Nayak <88781537+gautamnayak522@users.noreply.github.com>
2022-02-03 14:08:15 +0530

    commit for pull req

--- One.java
+++ One.java
@@ -4,6 +4,8 @@ public static void main(String[] arg) {
 for(;;)
 System.out.println("Welcome to alpha - ABCDEFGHIJKL!");
 System.out.println("Welcome to char ABCDEFGHIJKL!");
+
+System.out.println("this change made for pull req.");
 // To terminate this program press ctrl + c in the console.
 }
 }
